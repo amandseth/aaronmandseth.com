1626e75ec0a7b1882793b974416764febeb346f0
Aaron Mandseth <aaron@aaronmandseth.com>
2022-12-29 08:36:14 -0700

    Begin redesign, add starfield effect

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: {
+					foreground: "#f5f5f5",
+					"foreground-secondary": "#d8d8d8",
+					background: "#16181c",
+					"background-secondary": "#171e30",
+					highlight: "#4885eb",
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
