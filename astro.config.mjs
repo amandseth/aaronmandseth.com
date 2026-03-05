166053c9250ae4ec1bd4a118c84b18aca2cd7d1b
Aaron Mandseth <aaron@aaronmandseth.com>
2026-03-04 18:09:08 -0700

    Fix astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import preact from "@astrojs/preact";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({ compat: true })],
+  integrations: [tailwind(), preact({ compat: true }), icon()],
   vite: {
     ssr: {
       noExternal: ["react-hook-form"]
