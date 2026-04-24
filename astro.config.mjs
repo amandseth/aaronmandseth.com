c54e322a8d7edbe488578d28b6dc333afbb7e983
Aaron Mandseth <aaron@aaronmandseth.com>
2026-04-24 03:08:20 -0600

    Upgrade Astro and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  }
 });
